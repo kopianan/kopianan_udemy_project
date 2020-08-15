3e2e524a85e5c1bdd9c21fe25f5b54453b281854
ctrlaltdie <ananalfredcarlos@gmail.com>
2020-08-15 20:03:37 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/kopianan_udemy_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kopianan_udemy_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:kopianan_udemy_project/presentation/app_widget.dart';
+
+void main() {
+  runApp(AppWidget());
+}
